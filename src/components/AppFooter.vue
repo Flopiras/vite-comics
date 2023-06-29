0f9975e69ab2899f7d54f9fc2487010b6ac37c9e
Flopiras <Flopiras@yahoo.it>
2023-06-29 17:32:54 +0200

    divided bottom and top

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,5 +1,8 @@
 <script>
+import TopFooter from './TopFooter.vue';
+import BottomFooter from './BottomFooter.vue';
 export default {
+    components: { TopFooter, BottomFooter },
     data() {
         return {
         }
@@ -9,18 +12,8 @@ export default {
 
 <template>
     <footer>
-        <div class="container">
-            <nav></nav>
-            <figure>
-                <img src="../assets/img/dc-logo-bg.png" alt="logo">
-            </figure>
-        </div>
+        <TopFooter />
+        <BottomFooter />
     </footer>
 </template>
 
-<style lang="scss" scoped>
-footer {
-    background-image: url(../assets/img/footer-bg.jpg);
-    padding: 3rem 0;
-}
-</style>
